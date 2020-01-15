17ea4654d2fe4f60ab4df5fcae48e199746268f5
Colton Hester <colton.hester@improving.com>
2020-01-15 10:49:02 -0500

    initial commit

---
+++ ClamAV_LoadTest/Controllers/FileScanController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using nClam;
+
+namespace ClamAV_LoadTest.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FileScanController : ControllerBase
+	{
+		// GET api/filescan
+		[HttpGet]
+		public ActionResult<string> Get()
+		{
+			try
+			{
+				ClamClient clam = new ClamClient("localhost", 3310);
+				//string fileName1 = "C:\\Users\\ColtonHester\\Downloads\\eicar.com.txt";
+				//string fileName2 = "C:\\Users\\ColtonHester\\Pictures\\AssurantTest\\Test.pdf";
+				//string fileName1 = "C:\\Users\\ColtonHester\\Downloads\\nihongo_grammar_guide.pdf";
+				string fileName1 = "C:\\Users\\ColtonHester\\Downloads\\1126072019 Council Meeting Package_Externals.pdf";
+				string fileName2 = "C:\\Users\\ColtonHester\\Downloads\\eicar.com.txt";
+				Task<ClamScanResult> scanResult1 = clam.SendAndScanFileAsync(fileName1);
+				Task<ClamScanResult> scanResult2 = clam.SendAndScanFileAsync(fileName2);
+
+				string output = "FileScan on " + fileName1 + " results:" + scanResult1.Result.RawResult.Replace("stream:", "") + "\n" +
+					"FileScan on " + fileName2 + " results:" + scanResult2.Result.RawResult.Replace("stream:", "");
+
+				//using (System.IO.StreamWriter file =
+				//			new System.IO.StreamWriter(@"C:\\Users\\ColtonHester\\Downloads\\load_test\\log.txt", true))
+				//{
+				//	file.WriteLine(logoutput);
+				//}
+
+				return output;
+			}
+			catch (Exception ex) {
+				return ex.ToString();
+			}			
+		}
+	}
+}
